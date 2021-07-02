077e9e7b65c46bd84ba98feb4f06d8c3592ac321
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-07-02 12:01:03 +0530

    add store provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App">Hello World</div>;
-}
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
+import { StoreProvider } from "./store";
+import AppRoutes from "./routes";
+
+const App = () => {
+  return (
+    <StoreProvider>
+      <Router>
+        <Switch>
+          <Route component={AppRoutes} />
+        </Switch>
+      </Router>
+    </StoreProvider>
+  );
+};
 
 export default App;
