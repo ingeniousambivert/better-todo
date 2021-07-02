37c1e93ee2739a8ab4d865d1b1f9aa8a81149e2d
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-07-02 17:36:17 +0530

    models: add new data model

---
+++ server/models/todos.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    done: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const TodoModel = mongoose.model("todo", TodoSchema);
+
+module.exports = TodoModel;
