0ce8c4658d35c73066acbb5f92ad56def1c0d3b0
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-07-02 12:13:29 +0530

    add util methods

---
+++ client/src/utils/storage.js
@@ -0,0 +1,11 @@
+const storage = {
+  get(key) {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  },
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+};
+
+export default storage;
