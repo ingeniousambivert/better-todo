ec986e982a96a1c4fcc1f02b417165c785caec9f
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-07-02 18:27:48 +0530

    pages: update error handling

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -6,6 +6,7 @@ import ViewTodoList from "../../components/todo/view";
 import WriteTodo from "../../components/todo/write";
 
 function HomePage() {
+  const [loading, setLoading] = useState(false);
   const [showCreate, setShowCreate] = useState(false);
   const [showUpdate, setShowUpdate] = useState(false);
   const [currentTodo, setCurrentTodo] = useState(null);
@@ -17,7 +18,19 @@ function HomePage() {
   const auth = state.auth;
   const { id, accessToken } = auth;
 
+  const filterTodos = (e) => {
+    const search = e.target.value;
+    const filtered = [];
+    todos.forEach((todo) => {
+      if (todo.title.toLowerCase().includes(search.toLowerCase())) {
+        filtered.push(todo);
+      }
+    });
+    // dispatch({ type: "todos", filtered });
+  };
+
   const getTodos = () => {
+    setLoading(true);
     client
       .get("/api/todos", {
         headers: {
@@ -34,6 +47,7 @@ function HomePage() {
         console.log(error);
         setErorr(true);
       });
+    setLoading(false);
   };
 
   const createTodo = (data) => {
@@ -106,12 +120,18 @@ function HomePage() {
           <h1 className="text-2xl md:text-3xl text-gray-800 mb-3 mt-5 font-semibold">
             Your Todos
           </h1>
-          <div>
+          <div className="flex flex-row space-x-2">
+            <input
+              onChange={filterTodos}
+              placeholder="Search A Todo"
+              className="px-6 py-1 mt-4 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+
             <button
               onClick={() => {
                 setShowCreate(true);
               }}
-              className="block text-sm px-6 py-1 mt-4 text-white focus:outline-none bg-blue-700 rounded hover:bg-blue-600 transition duration-300"
+              className="text-sm px-6 py-1 mt-4 text-white focus:outline-none bg-blue-700 rounded hover:bg-blue-600 transition duration-300"
             >
               Add Todo
             </button>
@@ -144,6 +164,7 @@ function HomePage() {
                 />
               ) : (
                 <ViewTodoList
+                  loading={loading}
                   todos={todos}
                   setCurrentTodo={setCurrentTodo}
                   setShowUpdate={setShowUpdate}
