ba80106b584c7d8c25ba3d6b50847ceb45c0c4f0
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-07-04 10:31:59 +0530

    add bull board

--- server/app.js
+++ server/app.js
@@ -3,12 +3,20 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const requestLogger = require("morgan");
 
+const { createBullBoard } = require("bull-board");
+const { BullAdapter } = require("bull-board/bullAdapter");
+
 require("./config/mongoose");
 
+const { remindersQueue } = require("./queues/reminder");
 const usersRouter = require("./routes/users");
 const todosRouter = require("./routes/todos");
 
 const app = express();
+
+const { router } = createBullBoard([new BullAdapter(remindersQueue)]);
+
+app.use("/admin/queues", router);
 
 app.use(requestLogger("dev"));
 app.use(express.json());
