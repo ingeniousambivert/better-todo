7df67242b08310be03eeaa76c76b2254722304ae
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-07-06 20:11:52 +0530

    remove worker events

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const requestLogger = require("morgan");
-const logger = require("./utils/logger");
 
 const { createBullBoard } = require("bull-board");
 const { BullMQAdapter } = require("bull-board/bullMQAdapter");
@@ -10,7 +9,7 @@ const { BullMQAdapter } = require("bull-board/bullMQAdapter");
 require("./config/mongoose");
 
 const { remindersQueue } = require("./queues/reminder");
-const reminderWorker = require("./workers/reminder");
+
 const usersRouter = require("./routes/users");
 const todosRouter = require("./routes/todos");
 
@@ -19,19 +18,6 @@ const app = express();
 const { router } = createBullBoard([new BullMQAdapter(remindersQueue)]);
 
 app.use("/admin/queues", router);
-
-reminderWorker.on("error", (error) => {
-  logger.error(error);
-});
-
-reminderWorker.on("completed", (job, returnvalue) => {
-  console.log(returnvalue);
-  logger.info(`Completed job ${job.id} successfully`);
-});
-
-reminderWorker.on("failed", (job, failedReason) => {
-  logger.error(`Job ${job.id} failed. ${failedReason}`);
-});
 
 app.use(requestLogger("dev"));
 app.use(express.json());
