35933ddfa219baa1a1f9b4286f9341774fcaf1bc
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-07-06 20:17:38 +0530

    add worker listeners

---
+++ server/workers.js
@@ -0,0 +1,30 @@
+const { QueueScheduler } = require("bullmq");
+const reminderWorker = require("./workers/reminder");
+const logger = require("./utils/logger");
+
+const remindersQueueScheduler = new QueueScheduler("reminders", {
+  connection: {
+    host: "127.0.0.1",
+    port: 6379,
+  },
+});
+
+remindersQueueScheduler.on("failed", (error) => {
+  logger.error(error);
+  console.log("Failed processing reminders job", error);
+});
+
+reminderWorker.on("error", (error) => {
+  logger.error(error);
+});
+
+reminderWorker.on("completed", (job, returnvalue) => {
+  console.log(returnvalue);
+  logger.info(`Completed job ${job.id} successfully`);
+});
+
+reminderWorker.on("failed", (job, failedReason) => {
+  logger.error(`Job ${job.id} failed. ${failedReason}`);
+});
+
+console.log(`Started workers: ${reminderWorker.name}`);
